11767c084b45b93a55dedce53b3cbb9a0c225ee9
Natalia Romashev <tatasay@mail.ru>
2025-05-13 11:23:16 +0300

    Adding files: Makefiles, package-lock.json, package.json; folders: bin, node_modules

---
+++ bin/gendiff.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+.name('gendiff')
+.description('Compares two configuration files and shows a difference.')
+.version('1.0.0')
+.helpOption('-h, --help', 'output usage information')
+.option('-f, --format <type>', 'output format', 'stylish')
+.arguments('<filepath1> <filepath2>')
+.action((filepath1, filepath2, options) => {
+    
+})
+
+program.parse();
