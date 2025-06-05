90361f09bd644bef25f4497e1e98228fc79d6f57
Nataly <tatasay@mail.ru>
2025-06-05 22:46:51 +0300

    Update findDiff.js

--- src/findDiff.js
+++ src/findDiff.js
@@ -1,20 +1,20 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
 const findDiff = (data1, data2) => {
-  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
+  const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)))
 
   return keys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
+    const value1 = data1[key]
+    const value2 = data2[key]
 
     if (!_.has(data2, key)) {
-      return { key, type: 'removed', value: value1 };
+      return { key, type: 'removed', value: value1 }
     }
     if (!_.has(data1, key)) {
-      return { key, type: 'added', value: value2 };
+      return { key, type: 'added', value: value2 }
     }
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { key, type: 'nested', children: findDiff(value1, value2) };
+      return { key, type: 'nested', children: findDiff(value1, value2) }
     }
     if (!_.isEqual(value1, value2)) {
       return {
@@ -24,8 +24,8 @@ const findDiff = (data1, data2) => {
         value2,
       };
     }
-    return { key, type: 'unchanged', value: value1 };
-  });
-};
+    return { key, type: 'unchanged', value: value1 }
+  })
+}
 
-export default findDiff;
+export default findDiff
