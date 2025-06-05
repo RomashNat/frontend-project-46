d7ee3ee73cd0ca92916c0e78c970db370fa21bbc
Nataly <tatasay@mail.ru>
2025-06-05 19:17:37 +0300

    Add files via upload

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,27 +1,23 @@
 #!/usr/bin/env node
-
 import { Command } from 'commander';
 import genDiff from '../src/index.js';
-
 
 const program = new Command();
 
 program
-.name('gendiff')
-.description('Compares two configuration files and shows a difference.')
-.version('1.0.0')
-.helpOption('-h, --help', 'output usage information')
-.option('-f, --format <type>', 'output format', 'stylish')
-.arguments('<filepath1> <filepath2>')
-.action((filepath1, filepath2, options) => {
-    try { 
-        const result = genDiff(filepath1, filepath2, options.format);
-        console.log(result);
-    } catch(error) {
-        console.error('error: ', error.message);
-        process.exit(1);
+  .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2, options) => {
+    try {
+      const diff = genDiff(filepath1, filepath2, options.format);
+      console.log(diff);
+    } catch (error) {
+      console.error('Error:', error.message);
+      process.exit(1);
     }
-
-})
+  });
 
 program.parse(process.argv);
