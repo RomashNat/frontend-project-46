0170a8fc0e3863720e1c84999eacea445a2e6772
Nataly <tatasay@mail.ru>
2025-06-05 22:17:49 +0300

    Update buildDiff.js

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -1,20 +1,20 @@
-import _ from 'lodash';
+import _ from 'lodash'
 
 const buildDiff = (data1, data2) => {
   const keys = _.sortBy(_.union(Object.keys(data1), Object.keys(data2)));
 
   return keys.map((key) => {
-    const value1 = data1[key];
-    const value2 = data2[key];
+    const value1 = data1[key]
+    const value2 = data2[key]
 
     if (!_.has(data2, key)) {
-      return { key, type: 'removed', value: value1 };
+      return { key, type: 'removed', value: value1 }
     }
     if (!_.has(data1, key)) {
-      return { key, type: 'added', value: value2 };
+      return { key, type: 'added', value: value2 }
     }
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
-      return { key, type: 'nested', children: buildDiff(value1, value2) };
+      return { key, type: 'nested', children: buildDiff(value1, value2) }
     }
     if (!_.isEqual(value1, value2)) {
       return {
@@ -22,10 +22,10 @@ const buildDiff = (data1, data2) => {
         type: 'changed',
         value1,
         value2,
-      };
+      }
     }
-    return { key, type: 'unchanged', value: value1 };
-  });
-};
+    return { key, type: 'unchanged', value: value1 }
+  })
+}
 
-export default buildDiff;
+export default buildDiff
